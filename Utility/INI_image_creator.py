22afc639161b38c5980e9961449d028c91320c8d
Alex <atheinrich@outlook.com>
2025-09-30 12:45:55 -0700

    allows imported layouts and variants

---
+++ Utility/INI_image_creator.py
@@ -0,0 +1,298 @@
+########################################################################################################################################################
+# KLC to INI
+# PKL Application
+# Alex Heinrich
+########################################################################################################################################################
+
+########################################################################################################################################################
+# Imports
+from PIL import Image, ImageDraw, ImageFont
+
+########################################################################################################################################################
+# Parameters and data containers
+keyboard_layout = [ # default
+    ["~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "←"],
+    ["↹", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"],
+    ["⇪", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "↵"],
+    ["⇧", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "⇧"],
+    ["ctrl", "fn", "⊞", "alt", " ", "alt", "⊟", "ctrl"]]
+
+special_key_names = [
+    [], ["⇧"], ["ctrl"],
+    ["ctrl", "⇧"],
+    ["ctrl", "alt"], 
+    ["ctrl", "alt", "⇧"]]
+
+special_keys_width_by_index = {
+    (0, 13): 2,                                 # backspace
+    (1, 0):  1.5,   (1, 13): 1.5,               # tab, \
+    (2, 0):  1.75,  (2, 12): 2.42,              # caps lock, enter
+    (3, 0):  2.25,  (3, 11): 3.1,               # shift, shift
+    (4, 0):  1.08,  (4, 4):  6.25, (4, 7): 3.7} # ctrl, space, ctrl
+
+gray_keys = [
+    (1, 12), (1, 11),                           # [ ]
+    (2, 11), (2, 10),                           # ; '
+    (3, 10), (3, 9), (3, 8)]                    # , . /
+
+key_colors = {
+    "Regular":  "#D3D3D3",                      # letters
+    "Special":  "#A9A9A9",                      # commands and numbers
+    "Selected": "#808080"}                      # layout switch
+
+font              = ImageFont.truetype(r"cambria.ttc", 23, index=1)
+default_key_width = 30
+key_width         = 32
+key_height        = 32
+start_x           = 10
+start_y           = 10
+row_spacing       = 5
+col_spacing       = 5
+
+########################################################################################################################################################
+# Definitions
+def main():
+    
+    # Load KLC file
+    klc_name = input("Enter {filename}: ") + ".klc"
+    klc_list = load_klc(klc_name)
+    
+    # Extract data
+    keys, keys_list = klc_to_ini(klc_list)
+
+    # Create ini file
+    save_ini(keys)
+    
+    # Create images
+    layout_list = make_layouts(keys_list) # 0Norm, 1Sh, 2Ctrl, 6AGr, 7AGrSh, Caps
+    print(layout_list)
+    for i in range(len(layout_list)):
+        if len(layout_list[i]) > 2:
+            make_image(layout_list[i], i)
+
+def load_klc(klc_name):
+    """ Opens a KLC file as a list of lists with entries separated by tabs.
+    
+        output summary: [[SC, VK, blank, CapStat, 0Norm, 1Sh, 2Ctrl, 6AGr, 7AGrSh, Caps , ...], ...]
+        description:    [[UNICODE, other, other, other, NORMAL, SHIFT, CTRL, CTRL+SHIFT, CTRL+ALT, CTRL+ALT+SHIFT, ...], ...]
+        example:        [['02', '1', '', '5', '222b', '222e', '-1', '2124', '-1', '2115', '', ...], ...]
+    """
+
+    with open(klc_name, 'r', encoding='utf-16') as klc:
+        klc_list = list((line.strip().split('\t') for line in klc))
+    return klc_list
+
+def klc_to_ini(klc_list):
+    """ Formats extracted data to construct INI file.
+    
+        output summary: ["SC0## = VK	CapStat	0Norm	1Sh	2Ctrl	6AGr	7AGrSh	Caps	CapsSh", ...]
+        example:        ["SC002 = 1	5	∫	∮	--	ℤ	--	ℕ", ...]
+    """
+
+    # Initialize search through KLC data
+    keys, keys_list, trigger = [], [], 0
+    for i in range(len(klc_list)):
+        
+        # Identify first key
+        if klc_list[i][0] == "02":
+            trigger += 1
+        
+        # Extract data from line
+        if trigger == 1:
+        
+            # Initialize and set scan code and VK
+            key = f"SC0{klc_list[i][0]} = {klc_list[i][1]}\t"
+            key_list = [f"SC0{klc_list[i][0]}", klc_list[i][1]]
+            for j in range(2, 10):
+                if klc_list[i][j] != '':
+                
+                    if klc_list[i][j][0:2] == "//":
+                        break
+                    
+                    # Set CapStat
+                    if len(klc_list[i][j]) == 1:
+                        key += f"{klc_list[i][j]}\t"
+                        key_list.append(klc_list[i][j])
+                        
+                    # Set blank keys to --
+                    elif klc_list[i][j] == '-1':
+                        key += f"--\t"
+                        key_list.append("--")
+                    else:
+                    
+                        # Fixed unicode value
+                        if klc_list[i][j][-1] == "@":
+                            key += f"{chr(int(klc_list[i][j].replace('@', ''), 16))}\t"
+                            key_list.append(chr(int(klc_list[i][j].replace('@', ''), 16)))
+                        else:
+                        
+                            # Set key
+                            key += f"{chr(int(klc_list[i][j], 16))}\t"
+                            key_list.append(chr(int(klc_list[i][j], 16)))
+
+            # Identify last key
+            if klc_list[i][0] == "53": 
+                trigger -= 1
+            
+            # Add line to list for export
+            keys.append(key)
+            keys_list.append(key_list)
+    return keys, keys_list
+
+def save_ini(keys):
+    with open(f"layout.ini", 'w', encoding='utf-8') as output:
+        print("[layout]\n;scan = VK	CapStat	0Norm	1Sh	2Ctrl	6AGr	7AGrSh	Caps	CapsSh", file=output)
+        for i in range(len(keys)):
+            print(keys[i], file=output)
+
+def make_layouts(keys_list):
+    """ Formats extracted KLC data into the form given by keyboard_layout. """
+    
+    # Initialize keyboard variants
+    trigger = 0
+    layout_list_cache = [
+        [[],[],[],[],[]],
+        [[],[],[],[],[]],
+        [[],[],[],[],[]],
+        [[],[],[],[],[]],
+        [[],[],[],[],[]],
+        [[],[],[],[],[]]]
+    
+    # Set placeholder for problematic key
+    SC02b = []
+    
+    # Populate layout
+    for i in range(len(keys_list)):
+        if keys_list[i][0] == "SC002":   # first row (1, 2, 3, ...)
+            trigger = 0
+        elif keys_list[i][0] == "SC010": # second row (q, w, e, ...)
+            trigger = 1
+        elif keys_list[i][0] == "SC01e": # third row (a, s, d, ...)
+            trigger = 2
+        elif keys_list[i][0] == "SC02c": # fourth row (z, x, c, ...)
+            trigger = 3
+        elif keys_list[i][0] == "SC039":
+            break
+        
+        # Deal with problematic keys
+        if keys_list[i][0] == "SC02b":
+            SC02b = keys_list[i]
+            continue
+        elif keys_list[i][0] == "SC029":
+            continue
+        
+        # Append data
+        for j in range(0, len(keys_list[0])-3):
+            if keys_list[i][j+3] == "--":
+                layout_list_cache[j][trigger].append("")
+            else:
+                layout_list_cache[j][trigger].append(keys_list[i][j+3])
+                
+    # Deal with problematic keys
+    if SC02b:
+        for i in range(0, len(keys_list[0])-3):
+            if SC02b[i+3] == "--":
+                layout_list_cache[i][1].append("")
+            else:
+                layout_list_cache[i][1].append(SC02b[i+3])
+
+    # Fill in gaps an add special keys (ex. space, enter, ctrl, ...)
+    for i in range(len(layout_list_cache)):
+        if abs(len(layout_list_cache[i][0]) - 14):
+            layout_list_cache[i][0].insert(0, keyboard_layout[0][0])
+            for j in range(abs(len(layout_list_cache[i][0]) - 13)):
+                layout_list_cache[i][0].append('')
+            layout_list_cache[i][0].append(keyboard_layout[0][-1])
+        if abs(len(layout_list_cache[i][1]) - 14):
+            layout_list_cache[i][1].insert(0, keyboard_layout[1][0])
+            for j in range(abs(len(layout_list_cache[i][1]) - 14)):
+                layout_list_cache[i][1].append('')
+        if abs(len(layout_list_cache[i][2]) - 13):
+            layout_list_cache[i][2].insert(0, keyboard_layout[2][0])
+            for j in range(abs(len(layout_list_cache[i][2]) - 12)):
+                layout_list_cache[i][2].append('')
+            layout_list_cache[i][2].append(keyboard_layout[2][-1])            
+        if abs(len(layout_list_cache[i][3]) - 12):
+            layout_list_cache[i][3].insert(0, keyboard_layout[3][0])
+            for j in range(abs(len(layout_list_cache[i][3]) - 11)):
+                layout_list_cache[i][3].append('')
+            layout_list_cache[i][3].append(keyboard_layout[3][-1])
+        layout_list_cache[i][-1] = keyboard_layout[4]
+    
+    # Export data
+    return layout_list_cache
+
+def make_image(layout, index):
+    global font, start_x, start_y
+    
+    # Initialize parameters
+    font              = ImageFont.truetype(r"cambria.ttc", 23, index=1)
+    default_key_width = 30
+    key_width         = 32
+    key_height        = 32
+    start_x           = 10
+    start_y           = 10
+    row_spacing       = 5
+    col_spacing       = 5
+    
+    # Initialize image
+    keyboard_image = Image.new('RGB', (535, 200), color='#274829')
+    draw = ImageDraw.Draw(keyboard_image)
+
+    # Draw each key with its corresponding character
+    for row_index, row in enumerate(layout):
+        for key_index, key in enumerate(row):
+            key_width = special_keys_width_by_index.get((row_index, key_index), 1) * default_key_width
+            
+            # Determine key color by Regular or Special
+            key_color = key_colors["Special"] if (row_index, key_index) in special_keys_width_by_index else key_colors["Regular"]
+            if (row_index, key_index) in special_keys_width_by_index:
+                key_color = key_colors["Special"]
+            elif (row_index == 0) or (row_index == 4):
+                key_color = key_colors["Special"]
+            elif (row_index, key_index) in gray_keys:
+                key_color = key_colors["Special"]
+            else:
+                key_color = key_colors["Regular"]
+            
+            if key in special_key_names[index]:
+                key_color = key_colors["Selected"]
+            
+            # Fill rectangle with key color
+            draw.rectangle([start_x, start_y, start_x + key_width, start_y + key_height], fill=key_color, outline="black")
+            
+            if row_index == 4:
+                font = ImageFont.truetype(r"cambria.ttc", 15, index=1)
+            
+            # Calculate text size and position to center it
+            text_bbox = draw.textbbox((0, 0), key, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+            text_x = start_x + (key_width - text_width) / 2
+            text_y = start_y + (key_height - text_height) / 2
+            
+            draw.text((text_x, text_y), key, font=font, fill="black")
+            start_x += key_width + col_spacing
+        start_x = 10
+        start_y += key_height + row_spacing
+
+    # Save the image
+    if index == 0:
+        keyboard_image.save(f"state0.png")
+    elif index == 1:
+        keyboard_image.save(f"state1.png")
+    elif index == 2:
+        keyboard_image.save(f"state2.png") # ?
+    elif index == 3:
+        keyboard_image.save(f"state3.png")
+    elif index == 4:
+        keyboard_image.save(f"state6.png") # ?
+    elif index == 5:
+        keyboard_image.save(f"state7.png")
+    #keyboard_image.show()    
+
+########################################################################################################################################################
+# Global scripts
+main()
+
+########################################################################################################################################################
--- main.py
+++ main.py
@@ -6,13 +6,14 @@
 
 ##########################################################################################################
 # Imports
-import keyboard                 # Key replacement
-import tkinter as tk            # Image management
-from PIL import Image, ImageTk  # Image management
-import os                       # Image management
-import ctypes                   # Image management
-import importlib.util           # Image management
-import sys                      # Executable
+import keyboard                                # Key replacement
+import tkinter as tk                           # Image management
+from PIL import Image, ImageTk                 # Image management
+import os                                      # Image management
+import ctypes                                  # Image management
+import importlib.util                          # Image management
+import sys                                     # Executable
+import traceback                               # Debugging
 
 ##########################################################################################################
 # Parameters
@@ -26,45 +27,145 @@ screen_height = 0                              # Set later
 position_top  = False                          # Location of window
 
 ## Mapping
-current_layout = 0                             # Notes current layout
 replace        = False                         # Turns app on/off
+current_layout = 0                             # Notes current layout; alphanumeric order of Layout folders
+layout_state   = 0                             # Notes current variant; order of [0Norm, 1Sh]
+
+layout_states = ["0Norm", "1Sh"]
 
 ##########################################################################################################
 # Data
-## Data containers
-layouts       = [] # Holds layout dictionaries
-layout_images = [] # Holds image paths
-
-## Find directories
-def resource_path(relative_path):
-    if getattr(sys, 'frozen', False):
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
-layouts_path = resource_path("layouts")
-
-directories = []
-for directory in os.listdir(layouts_path):
-    path = os.path.join(layouts_path, directory)
-    if os.path.isdir(path): directories.append(path)
-
-## Fill data containers
-for d in sorted(directories):
+def _find_paths():
+    """ Returns the directory path for each layout folder in /root/Layouts. """
     
-    # Tkinter
-    img_path = os.path.join(d, "layout.png")
-    layout_images.append(img_path)
+    def resource_path(relative_path):
+        if getattr(sys, 'frozen', False):
+            return os.path.join(sys._MEIPASS, relative_path)
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(base_path, relative_path)
+    layouts_path = resource_path("layouts")
+
+    paths = []
+    for directory in os.listdir(layouts_path):
+        path = os.path.join(layouts_path, directory)
+        if os.path.isdir(path): paths.append(path)
     
-    # Mapping
-    data_path = os.path.join(d, "data.py")
-    spec = importlib.util.spec_from_file_location("data", data_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    layouts.append(module.mapping)
+    return paths
+
+class Layout:
+    """ Represents one layout folder. Processes replacement dictionaries and images. """
+
+    def __init__(self, dir_path):
+        
+        # Directories
+        self.path     = dir_path
+        self.name     = os.path.basename(self.path)
+        self.ini_path = os.path.join(self.path, "layout.ini")
+        
+        # Variants
+        self.variant_map = {
+            0: "0Norm",
+            1: "1Sh"}
+        self.vars = list(self.variant_map.values())
+
+        # Key bindings
+        self.mapping = {
+            self.vars[0]: {},
+            self.vars[1]: {}}
+        self._import_mapping()
+
+        # Images
+        self.images = {
+            self.vars[0]: None,
+            self.vars[1]: None}
+        self._import_images()
+
+    def _import_mapping(self):
+        """ Parse layout.ini and populate mapping for 0Norm and 1Sh. """
+        
+        # Handle non-alphabetic characters
+        VK_dict = {
+            "1":          ["1",  "!"],
+            "2":          ["2",  "@"],
+            "3":          ["3",  "#"],
+            "4":          ["4",  "$"],
+            "5":          ["5",  "%"],
+            "6":          ["6",  "^"],
+            "7":          ["7",  "&"],
+            "8":          ["8",  "*"],
+            "9":          ["9",  "("],
+            "0":          ["0",  ")"],            
+            "OEM_MINUS":  ["-",  "_"],
+            "OEM_PLUS":   ["=",  "+"],
+
+            "OEM_4":      ["[",  "{"],
+            "OEM_6":      ["]",  "}"],
+            "OEM_5":      ["\\", "|"],
+
+            "OEM_1":      [";",  ":"],
+            "OEM_7":      ["'",  '"'],
+            
+            "OEM_COMMA":  [",",  "<"],
+            "OEM_PERIOD": [".",  ">"],
+            "OEM_2":      ["/",  "?"],
+
+            "SPACE":      ["space",   "space"],
+            "DECIMAL":    ["decimal", "decimal"],
+            "OEM_3":      ["",   ""]}
+        
+        # Verify ini file exists
+        if not os.path.exists(self.ini_path):
+            print("(!) Run INI_image_creator in the layout folder.")
+            return
+        
+        # Extract all lines from the ini file
+        with open(self.ini_path, "r", encoding="utf-8") as file:
+            
+            # Process each row of data
+            for line in file.readlines():
+                
+                # Look for data
+                ## Skip first and second rows
+                if (not line) or line.startswith("[") or line.startswith(";"): continue
+
+                ## Select a row of data
+                _, right = line.split("=", 1)
+                parts = [part for part in right.split("\t") if part != ""]
+                parts = {
+                    "VK":    parts[0].strip(),
+                    "0Norm": parts[2],
+                    "1Sh":   parts[3]}
+                
+                # Partition columns as variants
+                for key, val in parts.items():
+                    if key in self.mapping.keys():
+                        
+                        # Handle non-alphabetic characters
+                        if parts["VK"] in VK_dict:
+                            if key == "0Norm": VK = VK_dict[parts["VK"]][0]
+                            else:              VK = VK_dict[parts["VK"]][1]
+                        
+                        # Handle alphabetic characters
+                        else: VK = parts["VK"].lower()
+                        
+                        # Handle blank characters
+                        if val == "--": val = parts["VK"]
+                        
+                        # Add to map
+                        self.mapping[key][VK] = val
+
+    def _import_images(self):
+        """ Find an image for each variant. """
+        
+        for idx, name in self.variant_map.items():
+            img_path = os.path.join(self.path, f"state{idx}.png")
+            if os.path.exists(img_path):
+                self.images[name] = Image.open(img_path)
 
 ##########################################################################################################
 # Tkinter
-## Functions
 def move_window():
+    """ Moves the window if the cursor overlaps with it. """
     global position_top
     
     # Find cursor location
@@ -81,25 +182,29 @@ def move_window():
     # Check if cursor is in window
     if (win_left <= cursor_x <= win_right) and (win_bottom <= cursor_y <= win_top):
         position_top = not position_top
-        place_window(root)
+        place_window()
     
     # Check again in 100 ms
     root.after(100, move_window)
 
-def place_window(window):
+def place_window():
+    """ Sets the location of the window. """
     global screen_width, screen_height
     
-    screen_width  = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
+    screen_width  = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
     x = (screen_width - img_width) // 2
     y = 0
     if not position_top: y = screen_height - img_height - lower_margin
-    window.geometry(f"{img_width}x{img_height}+{x}+{y}")
+    root.geometry(f"{img_width}x{img_height}+{x}+{y}")
 
-def show_window(index):
+def show_window():
+    """ Loads image and displays it in a window. """
     
     # Import layout image
-    img   = Image.open(layout_images[index])
+    if layout_state: state = 1
+    else:            state = 0
+    img   = layouts[current_layout].images[layout_states[state]]
     photo = ImageTk.PhotoImage(img)
     
     # Apply image to window
@@ -111,25 +216,16 @@ def show_window(index):
     raise_window()
 
 def raise_window():
+    """ Places the window above all else. """
+    
     root.lift()
     root.attributes("-topmost", True)
     root.after(1000, raise_window)
 
-## Initialization
-root = tk.Tk()               # Create window
-label = tk.Label(root)       # Initialize photo container
-label.pack()
-
-root.overrideredirect(True)  # Hide window minimize bar
-root.withdraw()              # Hide window at startup
-
-place_window(root)           # Set window size and location
-root.after(100, move_window) # Move window when cursor hovers over it
-
 ##########################################################################################################
 # Mapping
-## Functions
-def toggle_or_switch(event):
+def toggle_layout(event):
+    """ Toggles application and switches between layouts. """
     global replace, current_layout
     
     # Toggle replacement/layout
@@ -139,34 +235,78 @@ def toggle_or_switch(event):
         if keyboard.is_pressed('ctrl'):
             if replace:
                 current_layout = (current_layout + 1) % len(layouts)
-                show_window(current_layout)
+                show_window()
         
         # Toggle replacement
         else:
             replace = not replace
-            if replace: show_window(current_layout)
+            if replace: show_window()
             else:       root.withdraw()
 
 def key_replacement(event):
+    """ Handles replacement and switches between variants for the current layout. """
+    global layout_state
     
-    # Check for input
-    if (event.event_type == 'down') and replace:
+    if replace:
+        key = event.name.lower()
         
-        # Load layout
-        mapping = layouts[current_layout]
+        # Handle shift toggling
+        new_state = None
+        if (key == 'shift')   and (layout_state == 0): new_state = 1 # Queue shift
+        elif (key != 'shift') and (layout_state == 1): new_state = 2 # Note that shift has been used
+        elif (key == 'shift') and (layout_state == 2): new_state = 3
+        elif (key == 'shift') and (layout_state == 3): new_state = 0 # Check when shift is lifted
+        elif (key == 'shift') and (event.event_type == "up"): new_state = 0
+        if (new_state != layout_state) and (new_state is not None):
+            layout_state = new_state
+            show_window()
+        print(layout_state, key)
         
-        # Replace if necessary
-        if event.name in mapping:
-            keyboard.write(mapping[event.name], delay=0)
-            return False
+        # Check for input
+        if event.event_type == 'down':
+            
+            # Set layout
+            if layout_state: mapping = layouts[current_layout].mapping["1Sh"]
+            else:            mapping = layouts[current_layout].mapping["0Norm"]
+            
+            # Replace and update cache
+            if key in mapping:
+                keyboard.write(mapping[key], delay=0)
+                return False
     
     return True
 
-keyboard.hook(key_replacement, suppress=True)
-keyboard.hook_key("`", toggle_or_switch, suppress=True)
-
 ##########################################################################################################
 # Main
-root.mainloop()
+def main():
+    global root, layouts, label
+    
+    # Data
+    layouts = [Layout(path) for path in _find_paths()]
+    
+    # Tkinter
+    root = tk.Tk()               # Create window
+    label = tk.Label(root)       # Initialize photo container
+    label.pack()
+
+    root.overrideredirect(True)  # Hide window minimize bar
+    root.withdraw()              # Hide window at startup
+
+    place_window()               # Set window size and location
+    root.after(100, move_window) # Move window when cursor hovers over it
+    
+    # Mapping
+    keyboard.hook(key_replacement, suppress=True)
+    keyboard.hook_key("`", toggle_layout, suppress=True)
+
+    root.mainloop()
+    input("Press Enter to exit...")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        traceback.print_exc()
+        input("Press Enter to close...")
 
 ##########################################################################################################
